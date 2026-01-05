a97346c171b2b15b26a6ff0db14b3d95d76136e4
Polarian <polarian@polarian.dev>
2026-01-05 21:37:10 +0000

    Backup old config and verify config on load

--- src/main/java/xyz/crafttogether/craftcore/configuration/ConfigHandler.java
+++ src/main/java/xyz/crafttogether/craftcore/configuration/ConfigHandler.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import xyz.crafttogether.craftcore.CraftCore;
 
 import java.io.File;
+import java.nio.file.Files;
 
 public class ConfigHandler {
     private static final Logger logger = LoggerFactory.getLogger(ConfigHandler.class);
@@ -14,6 +15,7 @@ public class ConfigHandler {
     private static Config config;
 
     public static void loadConfig() {
+        verifyConfigVersion();  // Verify the config before loading it
         file = new File(CraftCore.getPlugin().getDataFolder() + "/config.yml");
         reloadConfig();
     }
@@ -46,9 +48,16 @@ public class ConfigHandler {
     private static void verifyConfigVersion() {
         if (CraftCore.getRequiredConfigVersion() == config.getConfigVersion()) return;
         logger.warn("Config version outdated, generating new config");
-        file.delete();
-        CraftCore.getPlugin().getConfig().options().copyDefaults();
-        CraftCore.getPlugin().saveDefaultConfig();
+        // Move the config before regenerating the config, adding .bak (backup) extension
+        boolean moved = file.renameTo(new File(file.getAbsolutePath() + ".bak"));
+        if (moved) {
+            logger.info("Created backup of config");
+            CraftCore.getPlugin().getConfig().options().copyDefaults();
+            CraftCore.getPlugin().saveDefaultConfig();
+        } else {
+            logger.error("Failed to create backup of config, aborting...");
+        }
+        logger.info("Unloaded CraftCore, please update config.yml and restart the server");
         CraftCore.unload();
     }
 
